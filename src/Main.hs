c5db94b47c17c74e42a1abe11a2b697c55e3186a
Carter Hinsley <carterhinsley@gmail.com>
2015-02-22 00:37:52 +0000

    (doc) Add Haddock doc for module Main

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,13 @@
--- Chop
--- Main.hs
+{-|
+Module      : Main
+Description : Entry point for parsebnf
+Copyright   : (c) Chop-lang, 2015
+License     : MIT
+Maintainer  : carterhinsley@gmail.com
+-}
+module Main where
 
+main :: IO ()
 main = do
     putStrLn "Hello world!"
 
